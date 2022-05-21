c92d2b8fcbe8174e013dc7f71e07af483ffd4f2a
tmwen <1255348874@qq.com>
2022-05-21 16:04:37 +0800

    数据库字段问题处理

--- laravel/database/seeds/ArticleCommentsSeeder.php
+++ laravel/database/seeds/ArticleCommentsSeeder.php
@@ -14,7 +14,7 @@ class ArticleCommentsSeeder extends Seeder
     {
         DB::table('article_comments')->insert([
             'user_id' => 1,
-            'artitle_id' => 1,
+            'article_id' => 1,
             'contents' => '这是一个很好的个人博客网站，运用了很多技术栈，值得学习',
             'create_time' => date('Y-m-d H:i:s'),
         ]);
